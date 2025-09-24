1494bdd9701912f5db85f594b45d9360422a0835
deepesh246 <deepeshgauro99@gmail.com>
2025-09-24 17:16:19 +0545

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,3 @@
+music={"ordinary":"https://www.youtube.com/watch?v=u2ah9tWTkmk",
+       "yellow":"https://www.youtube.com/watch?v=yKNxeF4KMsY",
+       "jogi":"https://www.youtube.com/watch?v=HexFqifusOk&list=RDGMEMCMFH2exzjBeE_zAHHJOdxgVMHexFqifusOk&start_radio=1&rv=yKNxeF4KMsY"}
